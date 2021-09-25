2b64510f24f019eb28dc1582024577eab788d29d
BaicoianuIoanSorin <71268639+BaicoianuIoanSorin@users.noreply.github.com>
2021-09-25 20:28:39 +0200

    BakeryStore
    BakeryStore
    new exercise

---
+++ Blazor/BakeryStore/Data/IProductsService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BakeryStore.Models;
+
+namespace BakeryStore.Data
+{
+    public interface IProductsService
+    {
+        public IList<Product> Products { get; }
+    }
+}
